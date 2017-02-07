99b6efca5aef224a11aca6f21262ebbd752a2259
Mohini Sindhu <mohini.sindhu14@gmail.com>
2017-02-07 18:57:01 +0530

    basic functionality of core data

--- MyStore/DeviceDetailViewController.m
+++ MyStore/DeviceDetailViewController.m
@@ -24,6 +24,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(NSManagedObjectContext *) managedObjectContext{
+	
+	
+	NSManagedObjectContext *context=nil;
+	id delegate=[[UIApplication sharedApplication]delegate];
+	if([delegate performSelector:@selector(managedObjectContext)]){
+		context=[delegate managedObjectContext];
+	}
+	return context;
+}
+
+
+
+
 /*
 #pragma mark - Navigation
 
@@ -35,8 +50,24 @@
 */
 
 - (IBAction)cancelButton:(id)sender {
+	[self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (IBAction)saveButton:(id)sender {
+	NSManagedObjectContext *context = [self managedObjectContext];
+	
+	// Create a new managed object
+	NSManagedObjectContext *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"Device" inManagedObjectContext:context];
+	[newDevice setValue:self.nameTextField.text forKey:@"name"];
+	[newDevice setValue:self.versionTextField.text forKey:@"version"];
+	[newDevice setValue:self.companyTextField.text forKey:@"company"];
+	
+	NSError *error = nil;
+	// Save the object to persistent store
+	if (![context save:&error]) {
+		NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+	}
+	
+	[self dismissViewControllerAnimated:YES completion:nil];
 }
 @end
